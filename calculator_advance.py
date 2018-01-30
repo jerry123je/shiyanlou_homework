890934e45ba87db55d4151bb79ec094494a7db4f
jerry123jerry <jerry123jerry@163.com>
2018-01-30 13:41:56 +0800

    challenge 2 final

--- calculator_advance.py
+++ calculator_advance.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 
 import sys
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print("Usage: %s [500]"%sys.argv[0])
     exit()
 
 persons = sys.argv[1:]
+ 
+#stuff[person_id] = [salary, insurance, tax, salary_after]
 stuff = {}
 for person in persons:
     try:
         person_id,salary = person.split(':')
-        stuff[int(person_id)] = int(salary)
+        stuff[int(person_id)] = [int(salary)]
     except ValueError:
         print("Parameter Error")
 
-print(stuff)
+#print(stuff)
 
-def salary_cal(salary,insurance):
+def salary_cal(person_id):
+    salary = stuff[person_id][0]
     insurance = salary * 0.08 + salary * 0.02 + salary * 0.005 + salary * 0.06
+    stuff[person_id].append(format(insurance,'.2f'))
     tax_count = salary - insurance - 3500
     if tax_count <= 0:
         tax = 0
@@ -35,8 +39,11 @@ def salary_cal(salary,insurance):
         tax = tax_count * 0.35 - 5505
     else:
         tax = tax_count * 0.45 - 13505
-    tax = format(tax, ".2f")
-    return tax
+    stuff[person_id].append(format(tax, ".2f"))
+    stuff[person_id].append(format(salary - insurance - tax, '.2f'))
+    print("%s:%s"%(person_id,stuff[person_id][3]))
 
-tax = salary_cal(salary,insurance)
-print(tax) 
+if __name__ == '__main__':
+    for person_id in stuff.keys():
+        salary_cal(person_id)
+
