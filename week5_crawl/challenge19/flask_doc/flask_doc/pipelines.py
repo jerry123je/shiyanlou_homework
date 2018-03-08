c2baf20aa4ae0e794c18fc516eda0f6c20111254
Jerry <jerry.ji@sonymobile.com>
2018-03-08 14:32:06 +0800

    chanllenge19 0.1

---
+++ week5_crawl/challenge19/flask_doc/flask_doc/pipelines.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import re
+import redis
+import json
+
+
+class FlaskDocPipeline(object):
+    def process_item(self, item, spider):
+        item['text'] = re.sub(r'\n{2,}','\n',re.sub(r'<[^>]+>|[ ]{2,}','',item['text']))
+        self.redis.lpush('flask_doc:items',item)
+        return item
+
+    def open_spider(self, spider):
+        self.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+    def close_spider(self, spider):
+        self.redis.close()
