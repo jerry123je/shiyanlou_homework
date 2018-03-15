f7c197c6cf91d53ed9317e1cc15cf9d00ed87d61
Jerry <jerry.ji@sonymobile.com>
2018-03-15 16:39:25 +0800

    challenge23 0.9

---
+++ week6_data/challenge23/datafigure.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def data_plot():
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title("StudyData")
+    major_ticks_y = np.arange(0,3001,500)
+    major_ticks_x = np.arange(0,200001,50000)
+
+    ax.set_xticks(major_ticks_x)
+    ax.set_yticks(major_ticks_y)
+
+    ax.set_xlabel("User ID")
+    ax.set_ylabel("Study Time")
+
+    files = open('user_study.json').read()
+
+    data = pd.read_json(files)
+
+    data1 = data[['user_id','minutes']].groupby('user_id').sum()
+    
+    data1.plot(ax=ax)
+
+    plt.show()
+    return ax
+
+data_plot()
