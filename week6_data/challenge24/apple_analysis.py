a5566f2ec3e41692d129ebeb02d1a769b64e0c4d
jerry.ji <jerry123jerry@163.com>
2018-03-19 07:05:07 +0800

    challenge24 0.9

---
+++ week6_data/challenge24/apple_analysis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+def quarter_volumn():
+	data = pd.read_csv('apple.csv', parse_dates=['Date'], index_col=['Date'])
+	quarter = data.resample('Q').sum()
+	quarter_sort = quarter.sort_values(axis=0, ascending=False, by='Volume')
+	second_volume = quarter_sort.ix[1]['Volume']
+	
+	return second_volume
+
+
+print(quarter_volumn())
